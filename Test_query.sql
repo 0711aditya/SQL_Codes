23cbf191f0f1337345f5bb50db19c1776852899b
0711aditya <131670560+0711aditya@users.noreply.github.com>
2024-03-13 15:19:52 +0530

    Update Test_query.sql

--- Test_query.sql
+++ Test_query.sql
@@ -6,3 +6,12 @@ ORDER BY last_name DESC, first_name DESC
 
 ----------------------------------------------- 
 
+
+SELECT
+first_name, last_name, email
+FROM customer
+ORDER BY last_name DESC, first_name ASC 
+
+
+------------------------------------------------
+
